782cf82923eef74cda64f19d6e5f6b5d209786ab
Joshua Coleman <jlcolema@me.com>
2020-08-13 11:17:36 -0500

    Add starter markup for Destinations page.

--- dist/html/wp-content/themes/rtggolf/page-destinations.php
+++ dist/html/wp-content/themes/rtggolf/page-destinations.php
@@ -1,21 +1,153 @@
 <?php get_header(); ?>
 
-	<h1 class=""><?php the_title(); ?></h1>
+	<?php /* Banner */ ?>
 
-	<!-- Banner -->
+	<div class="banner">
 
-	Destinations
+		<div class="banner__inner-wrap">
 
-	[Image]
+			<h1 class="">Destinations</h1>
 
-	<!-- List of Destinations -->
+		</div>
 
-	Title
+	</div>
 
-	Short Description
+	<?php /* List of Destinations */ ?>
 
-	Learn More
+	<div class="">
 
-	[Image]
+		<div class="">
+
+			<div class="">
+
+				<ul class="">
+
+					<li class="">
+
+						<div class="">
+
+							<img src="https://via.placeholder.com/400x200" alt="A very nice description." class="" />
+
+						</div>
+
+						<h2 class="">Title</h2>
+
+						<p class="">A short description of the trip.</p>
+
+						<div class="">
+
+							<a href="/destinations/title/" class="">Learn More</a>
+
+						</div>
+
+					</li>
+
+					<li class="">
+
+						<div class="">
+
+							<img src="https://via.placeholder.com/400x200" alt="A very nice description." class="" />
+
+						</div>
+
+						<h2 class="">Title</h2>
+
+						<p class="">A short description of the trip.</p>
+
+						<div class="">
+
+							<a href="/destinations/title/" class="">Learn More</a>
+
+						</div>
+
+					</li>
+
+					<li class="">
+
+						<div class="">
+
+							<img src="https://via.placeholder.com/400x200" alt="A very nice description." class="" />
+
+						</div>
+
+						<h2 class="">Title</h2>
+
+						<p class="">A short description of the trip.</p>
+
+						<div class="">
+
+							<a href="/destinations/title/" class="">Learn More</a>
+
+						</div>
+
+					</li>
+
+					<li class="">
+
+						<div class="">
+
+							<img src="https://via.placeholder.com/400x200" alt="A very nice description." class="" />
+
+						</div>
+
+						<h2 class="">Title</h2>
+
+						<p class="">A short description of the trip.</p>
+
+						<div class="">
+
+							<a href="/destinations/title/" class="">Learn More</a>
+
+						</div>
+
+					</li>
+
+					<li class="">
+
+						<div class="">
+
+							<img src="https://via.placeholder.com/400x200" alt="A very nice description." class="" />
+
+						</div>
+
+						<h2 class="">Title</h2>
+
+						<p class="">A short description of the trip.</p>
+
+						<div class="">
+
+							<a href="/destinations/title/" class="">Learn More</a>
+
+						</div>
+
+					</li>
+
+					<li class="">
+
+						<div class="">
+
+							<img src="https://via.placeholder.com/400x200" alt="A very nice description." class="" />
+
+						</div>
+
+						<h2 class="">Title</h2>
+
+						<p class="">A short description of the trip.</p>
+
+						<div class="">
+
+							<a href="/destinations/title/" class="">Learn More</a>
+
+						</div>
+
+					</li>
+
+				</ul>
+
+			</div>
+
+		</div>
+
+	</div>
 
 <?php get_footer(); ?>
