019006b1eac93c7b009c5c9c0c46ba0c45690a4a
Joshua Coleman <jlcolema@me.com>
2020-08-13 11:12:57 -0500

    Add starter markup for The Experience page.

--- dist/html/wp-content/themes/rtggolf/page-the-experience.php
+++ dist/html/wp-content/themes/rtggolf/page-the-experience.php
@@ -1,36 +1,68 @@
 <?php get_header(); ?>
 
-	<h1 class=""><?php the_title(); ?></h1>
-
 	<!-- Banner -->
 
-	The Experience
+	<div class="banner">
 
-	[Image]
+		<div class="banner__inner-wrap">
 
-	<!-- Title -->
+			<h1 class="">The Experience</h1>
 
-	The Experience
+		</div>
 
-	RTG will take your golf trip from concept to completion with attention to every detail in between.
+	</div>
 
-	"As golfers that go on these golf trips ourselves, we understand the unique requirements that are peculiar to golfers and their trips."
+	<?php /* Title */ ?>
 
-	[Image]
+	<div class="">
 
-	RTG will take your golf trip from concept to completion with attention to every detail in between.
+		<div class="">
 
-	-- Detailed proposals delivered online in a format that is easy to share with fellow travelers
-	-- Online Registration and payments
-	-- Individual trip websites
-	-- Detailed trip itineraries
-	-- All your trip details at your finger tips while you travel
-	-- Competition Design and real-time scoring
+			The Experience
 
-	RTG Golf is expert in assisting trip leaders with moving their trip -- from "great idea to looking "on the calendar".
+			RTG will take your golf trip from concept to completion with attention to every detail in between.
 
-	We understand that your trip is full up until the minute the first payment is due! We have discovered that if you provide your golf pals with: dates, destination and price -- they will have all the information they need to accept or decline the invitation.
+			"As golfers that go on these golf trips ourselves, we understand the unique requirements that are peculiar to golfers and their trips."
 
+		</div>
 
+	</div>
+
+	<?php /* Title */ ?>
+
+	<div class="">
+	
+		<div class="">
+
+			<div class="">
+
+				<img src="https://via.placeholder.com/400x400" alt="A very nice description." class="" />
+
+			</div>
+
+			<div class="">
+
+				RTG will take your golf trip from concept to completion with attention to every detail in between.
+
+				<ul class="">
+
+					<li class="">Detailed proposals delivered online in a format that is easy to share with fellow travelers</li>
+					<li class="">Online Registration and payments</li>
+					<li class="">Individual trip websites</li>
+					<li class="">Detailed trip itineraries</li>
+					<li class="">All your trip details at your finger tips while you travel</li>
+					<li class="">Competition Design and real-time scoring</li>
+
+				</ul>
+
+				RTG Golf is expert in assisting trip leaders with moving their trip -- from "great idea to looking "on the calendar".
+
+				<p><i>We understand that your trip is full up until the minute the first payment is due! We have discovered that if you provide your golf pals with: dates, destination, and price -- they will have all the information they need to accept or decline the invitation.</i></p>
+
+			</div>
+
+		</div>
+
+	</div>
 
 <?php get_footer(); ?>
