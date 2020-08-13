295be693b04a4b8414bea5515d532a1016b40493
Joshua Coleman <jlcolema@me.com>
2020-08-13 10:46:07 -0500

    Add starter markup for About Us page.

--- dist/html/wp-content/themes/rtggolf/page-about-us.php
+++ dist/html/wp-content/themes/rtggolf/page-about-us.php
@@ -1,61 +1,179 @@
 <?php get_header(); ?>
 
-	<h1 class=""><?php the_title(); ?></h1>
+	<?php /* Banner */ ?>
 
-	<!-- Banner -->
+	<div class="banner">
 
-	About Us
+		<div class="banner__inner-wrap">
 
-	[Image]
+			<h1 class="">About Us</h1>
 
-	<!-- Title -->
+		</div>
 
-	Another Trip of a Lifetime
+	</div>
 
-	Professional Design and Management of Golf Travel
+	<?php /* Title */ ?>
 
-	At RTG Golf our business is the professional design and management of golf travel. We are expert at tying together all the details involved in the planning and execution of great golf trips. From site selection, accommodations and expense management to creating and scoring the appropriate competition.
+	<div class="">
 
-	RTG Golf is a Twin Cities based business established in 1999. Since that time we have sent thousands of golf travelers to destinations around the globe. We have tremendous experience in both domestic and international destinations – with a particular expertise in the British Isles.
+		<div class="">
 
-	We are golfers that go on golf trips and understand the nuances that transform golf trips from Good to Great!
+			<h2 class="">Another Trip of a Lifetime</h2>
 
-	<!-- Title -->
+			<h3 class="">Professional Design and Management of Golf Travel</h3>
 
-	The Experience
+			<div class="">
 
-	Taking Golf Trips from Good to Great
+				<p>At RTG Golf our business is the professional design and management of golf travel. We are expert at tying together all the details involved in the planning and execution of great golf trips. From site selection, accommodations and expense management to creating and scoring the appropriate competition.</p>
 
-	Anyone can book a golf trip. At RTG Golf we take your golf trip from good to great by eliminating the trial and error of finding the right courses, accommodations, transportation, restaurants, pubs, sights, etc.
+				<p>RTG Golf is a Twin Cities based business established in 1999. Since that time we have sent thousands of golf travelers to destinations around the globe. We have tremendous experience in both domestic and international destinations – with a particular expertise in the British Isles.</p>
 
-	We have not only been to the destinations we sell, we have sent thousands of travelers to the same. Our first hand experience and feedback from our clients keeps us in the know and up to date on great golf travel.
+				<p>We are golfers that go on golf trips and understand the nuances that transform golf trips from Good to Great!</p>
 
-	[Image]
+			</div>
 
-	<!-- Partners -->
+		</div>
 
-	[Image]
+	</div>
 
-	Tom Kozlak
+	<?php /* Title */ ?>
 
-	Partner
+	<div class="">
 
-	RTG Golf is a second career for me. In 1999, after 25 years in the restaurant business I decided I wanted to explore a second career. Still at it 20 years later.
+		<div class="">
 
-	In that 20 years I have “researched” golf all over the world and have gained the expertise to create great golf itineraries domestically and internationally.
+			<div class="">
 
-	[Image]
+				<img src="https://via.placeholder.com/400x400" alt="A very nice description." class="" />
 
-	Scott Cole
+			</div>
 
-	Partner
+			<h2 class="">The Experience</h2>
 
-	Scott Cole has been with RTG Golf for three years. Scott has been in the golf business for 28 years. Golf has been in Scott’s blood since the age of 4. After winning two Wisconsin State High School Individual Championships and two Team Championships, he went on to play college golf at University of Southern Mississippi. After a year and a half, he transferred to Marquette University to play on the golf team and graduate in 1999.
+			<h3 class="">Taking Golf Trips from Good to Great</h3>
 
-	In 2001, Scott turned professional and played mini-tours in Florida until March of 2002 when he became the Head Golf Professional at New Richmond Golf Club. Scott spent 13 years as the Head Golf Professional at New Richmond Golf Club. Scott participated in the 2013 and 2014 National PGA Club Pro Championship. In 2014, Scott accepted the Head Professional job at Stoneridge Golf Club. Scott participated in the 2013 and 2014 National Club Pro Championship. After three years at Stoneridge Golf Club, it was time for change. In May of 2017, Scott came onboard at RTG Golf.
+			<div class="">
 
-	Scott currently resides in New Richmond, Wisconsin with his wife Katie and daughters, Kenley and Gracie.
+				<p>Anyone can book a golf trip. At RTG Golf we take your golf trip from good to great by eliminating the trial and error of finding the right courses, accommodations, transportation, restaurants, pubs, sights, etc.</p>
 
-	<!-- Testimonials -->
+				<p>We have not only been to the destinations we sell, we have sent thousands of travelers to the same. Our first hand experience and feedback from our clients keeps us in the know and up to date on great golf travel.</p>
+
+				<div class="">
+
+					<img src="https://via.placeholder.com/300x100" alt="Signature" class="" />
+
+				</div>
+
+			</div>
+
+		</div>
+
+	</div>
+					
+	<?php /* Partners */ ?>
+
+	<div class="">
+
+		<div class="">
+
+			<div class="">
+
+				<ul class="">
+
+					<li class="">
+
+						<div class="">
+
+							<img src="https://via.placeholder.com/300x300" alt="Tom Kozlak" class="" />
+
+						</div>
+
+						<h2 class="">Tom Kozlak</h2>
+
+						<h3 class="">Partner</h3>
+
+						<div class="">
+
+							<p>RTG Golf is a second career for me. In 1999, after 25 years in the restaurant business I decided I wanted to explore a second career. Still at it 20 years later.</p>
+
+							<p>In that 20 years I have “researched” golf all over the world and have gained the expertise to create great golf itineraries domestically and internationally.</p>
+
+						</div>
+
+					</li>
+
+					<li class="">
+
+						<div class="">
+
+							<img src="https://via.placeholder.com/300x300" alt="Scott Cole" class="" />
+
+						</div>
+
+						<h2 class="">Scott Cole</h2>
+
+						<h3 class="">Partner</h3>
+
+						<div class="">
+							
+							<p>Scott Cole has been with RTG Golf for three years. Scott has been in the golf business for 28 years. Golf has been in Scott’s blood since the age of 4. After winning two Wisconsin State High School Individual Championships and two Team Championships, he went on to play college golf at University of Southern Mississippi. After a year and a half, he transferred to Marquette University to play on the golf team and graduate in 1999.</p>
+
+							<p>In 2001, Scott turned professional and played mini-tours in Florida until March of 2002 when he became the Head Golf Professional at New Richmond Golf Club. Scott spent 13 years as the Head Golf Professional at New Richmond Golf Club. Scott participated in the 2013 and 2014 National PGA Club Pro Championship. In 2014, Scott accepted the Head Professional job at Stoneridge Golf Club. Scott participated in the 2013 and 2014 National Club Pro Championship. After three years at Stoneridge Golf Club, it was time for change. In May of 2017, Scott came onboard at RTG Golf.</p>
+
+							<p>Scott currently resides in New Richmond, Wisconsin with his wife Katie and daughters, Kenley and Gracie.</p>
+
+						</div>
+
+					</li>
+
+				</ul>
+
+			</div>
+
+		</div>
+
+	</div>
+
+	<?php /* Testimonials */ ?>
+
+	<div class="">
+
+		<div class="">
+
+			<h2 class="">Testimonials</h2>
+
+			<div class="">
+
+				<ul class="">
+
+					<li class="">
+
+						<blockquote class="">
+
+							<div class="">
+
+								<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti et iste eius! Excepturi, recusandae laboriosam! Ipsum facere molestias eum iure esse pariatur nulla, similique sit porro necessitatibus fugit eligendi quos.</p>
+
+							</div>
+
+							<span class="">
+
+								<strong class="">Jane Appleseed</strong>
+								
+								<i class="">CEO</i>
+
+							</span>
+
+						</blockquote>
+
+					</li>
+
+				</ul>
+
+			</div>
+
+		</div>
+
+	</div>
 
 <?php get_footer(); ?>
