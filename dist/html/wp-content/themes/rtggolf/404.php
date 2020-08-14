ea52e96bec9ed18dd605e1f0b9f2486b41aff9d9
Joshua Coleman <jlcolema@me.com>
2020-08-14 12:18:40 -0500

    Add starter 404 page.

--- dist/html/wp-content/themes/rtggolf/404.php
+++ dist/html/wp-content/themes/rtggolf/404.php
@@ -8,19 +8,11 @@
  */
 
 get_header();
+
 ?>
 
-	<h1 class="entry-title"><?php _e( 'Page Not Found', 'rtggolf' ); ?></h1>
-
-	<div class="intro-text"><p><?php _e( 'The page you were looking for could not be found. It might have been removed, renamed, or did not exist in the first place.', 'rtggolf' ); ?></p></div>
-
-		<?php
-		get_search_form(
-			array(
-				'label' => __( '404 not found', 'rtggolf' ),
-			)
-		);
-		?>
+	<h1 class="">404</h1>
 
 <?php
+
 get_footer();
