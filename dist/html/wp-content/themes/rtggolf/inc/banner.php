ce6cac20cfb94506b882c81be603585bdae57e8c
Joshua Coleman <jlcolema@me.com>
2020-08-17 11:33:21 -0500

    Move banner, hero, and logo to be includes files to easier use.

---
+++ dist/html/wp-content/themes/rtggolf/inc/banner.php
@@ -0,0 +1,19 @@
+<div class="banner">
+
+	<div class="banner__inner-wrap">
+
+		<h1 class="banner__title"><?php the_title(); ?></h1>
+
+		<picture class="banner__picture">
+
+			<source srcset="https://via.placeholder.com/1600x300" media="(min-width: 1000px)">
+
+			<source srcset="https://via.placeholder.com/1200x300" media="(min-width: 700px)">
+
+			<img src="https://via.placeholder.com/1000x300" alt="A very nice description." class="banner__img">
+
+		</picture>
+
+	</div>
+
+</div>
---
+++ dist/html/wp-content/themes/rtggolf/inc/hero.php
@@ -0,0 +1,43 @@
+<div class="hero">
+
+	<div class="hero__inner-wrap">
+
+		<div class="hero__cta">
+
+			<div class="hero__label">RTG Golf</div>
+
+			<h1 class="hero__header">Another Trip of a Lifetime</h1>
+
+			<div class="hero__details">
+
+				<p>We take golf trips from good to great!</p>
+
+			</div>
+
+			<div class="hero__more">
+
+				<a href="/about-us/" class="hero__link">Learn More</a>
+
+			</div>
+
+		</div>
+
+		<div class="hero__provider">
+
+			<span class="">St. Andrews Links Authorized Provider</span>
+
+		</div>
+
+		<picture class="hero__picture">
+
+			<source srcset="https://via.placeholder.com/1600x500" media="(min-width: 1000px)">
+
+			<source srcset="https://via.placeholder.com/1200x500" media="(min-width: 700px)">
+
+			<img src="https://via.placeholder.com/1000x500" alt="A very nice description." class="hero__img">
+
+		</picture>
+
+	</div>
+
+</div>
