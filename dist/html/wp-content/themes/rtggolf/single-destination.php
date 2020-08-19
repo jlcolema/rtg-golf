ae1f5647869c7e2da895e66fc78545e7c056063d
Joshua Coleman <jlcolema@me.com>
2020-08-19 15:00:55 -0500

    Remove banner.

--- dist/html/wp-content/themes/rtggolf/single-destination.php
+++ dist/html/wp-content/themes/rtggolf/single-destination.php
@@ -1,17 +1,5 @@
 <?php get_header(); ?>
 
-	<?php /* Banner */ ?>
-
-	<div class="banner">
-
-		<div class="banner__inner-wrap">
-
-			<h1 class=""><?php the_title(); ?></h1>
-
-		</div>
-
-	</div>
-
 	<?php /* Overview */ ?>
 
 	<div class="">
