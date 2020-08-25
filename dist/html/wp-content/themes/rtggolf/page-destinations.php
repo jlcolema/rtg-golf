2c90fdaa9fd2b4e2b5b479763ec2e83f3a196c3a
Joshua Coleman <jlcolema@me.com>
2020-08-25 13:39:57 -0500

    Connect preview images for destination entries to WordPress.

--- dist/html/wp-content/themes/rtggolf/page-destinations.php
+++ dist/html/wp-content/themes/rtggolf/page-destinations.php
@@ -33,15 +33,15 @@
 
 					// Attachment
 
-					$destination_thumbnail_attachment_id = get_field( 'destination_thumbnail', $destination->ID );
+					$destination_preview_image_attachment_id = get_field( 'destination_preview_image', $destination->ID );
 
 					// Size
 
-					$destination_thumbnail_size_full = 'full';
+					$destination_preview_image_size_full = 'full';
 
 					// Options
 
-					$destination_thumbnail_full = wp_get_attachment_image_src( $destination_thumbnail_attachment_id, $destination_thumbnail_size_full );
+					$destination_preview_image_full = wp_get_attachment_image_src( $destination_preview_image_attachment_id, $destination_preview_image_size_full );
 
 				?>
 
@@ -49,11 +49,11 @@
 
 					<picture class="destinations__picture">
 
-						<source srcset="https://via.placeholder.com/960x400" media="(min-width: 1000px)">
+						<source srcset="<?php echo $destination_preview_image_full[0]; ?>" media="(min-width: 1000px)">
 
-						<source srcset="https://via.placeholder.com/9600x400" media="(min-width: 700px)">
+						<source srcset="<?php echo $destination_preview_image_full[0]; ?>" media="(min-width: 700px)">
 
-						<img src="https://via.placeholder.com/960x400" alt="A very nice description." class="destinations__img">
+						<img src="<?php echo $destination_preview_image_full[0]; ?>" alt="A very nice description." class="destinations__img">
 
 					</picture>
 
