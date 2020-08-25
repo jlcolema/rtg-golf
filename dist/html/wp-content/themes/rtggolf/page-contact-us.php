499261ad3bc39257a1551f053fc0f663c3b8ea64
Joshua Coleman <jlcolema@me.com>
2020-08-25 09:54:07 -0500

    Add placeholder detail text for form.

--- dist/html/wp-content/themes/rtggolf/page-contact-us.php
+++ dist/html/wp-content/themes/rtggolf/page-contact-us.php
@@ -8,6 +8,8 @@
 
 			<h2 class="">Please Get in Touch</h2>
 
+			<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci sunt quos aliquid cumque ab voluptatum vel expedita debitis quasi doloremque ut hic eum quis, rerum cum labore animi provident qui.</p>
+
 			<?php echo do_shortcode( '[contact-form-7 id="27" title="Contact"]' ); ?>
 
 		</div>
