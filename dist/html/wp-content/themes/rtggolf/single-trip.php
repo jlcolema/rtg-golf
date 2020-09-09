a3ef5eeadd379fe75fc4c1f7e9bb934639e88090
Joshua Coleman <jlcolema@me.com>
2020-09-09 10:36:46 -0500

    Connect Overview, Travefy, and Itinerary sections to the system.

--- dist/html/wp-content/themes/rtggolf/single-trip.php
+++ dist/html/wp-content/themes/rtggolf/single-trip.php
@@ -2,79 +2,21 @@
 
 	<!-- Overview -->
 
-	<div class="trip__overview">
+	<?php $trip_overview = get_field( 'trip_overview' ); ?>
 
-		<div class="inner-wrap trip__overview-inner-wrap">
+	<?php if ( $trip_overview ) : ?>
 
-			<h2 class="trip__overview-header">Ireland Overview</h2>
+		<div class="trip__overview">
 
-			<h3 class="trip__overview-subheader">RTG Golf has traveled extensively in Ireland over the past 20 years developing itineraries from North to South and East to West</h3>
+			<div class="inner-wrap trip__overview-inner-wrap">
 
-			<p class="trip__overview-description">Play The Greats, Get Off The Beaten Path or a combination of the two--RTG will design the trip you are looking for!</p>
+				<h2 class="trip__overview-header"><?php echo $trip_overview[ 'trip_overview_header' ]; ?></h2>
 
-		</div>
+				<h3 class="trip__overview-subheader"><?php echo $trip_overview[ 'trip_overview_subheader' ]; ?></h3>
 
-	</div>
+				<div class="trip__overview-description">
 
-	<!-- Travefy -->
-
-	<div class="trip__travefy">
-
-		<div class="inner-wrap trip__travefy-inner-wrap">
-
-			<!-- Use unique ID for content entry -->
-
-			<!-- <iframe src="https://travefy.com/trip/6yw9rqt2mbmwqz2auy7m5xw4932dpfq" class="trip__travefy-iframe"></iframe> -->
-
-			<!-- Use the following... -->
-
-			<div data-travefy-itinerary-id="6yw9rqt2mbmwqz2auy7m5xw4932dpfq" class="travefy-itinerary travefy__itinerary"></div>
-
-			<!-- Add this script to pages that have the data-travefy-itinerary-id code above -->
-
-			<script data-version="v0" id="travefy-itinerary-embedder" type="text/javascript"> (function () { function l() { var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = '//travefy.com/embed/itinerary.1.0.js'; var e = document.getElementById('travefy-itinerary-embedder'); e.parentNode.insertBefore(s, e); } if (window.attachEvent) { window.attachEvent('onload', l); } else { window.addEventListener('load', l, false); } })(); </script>
-
-		</div>
-
-	</div>
-
-	<!-- Itenerary -->
-
-	<div class="trip__itinerary">
-
-		<div class="inner-wrap trip__itinerary-inner-wrap">
-
-			<div class="trip__itinerary-image">
-
-				<picture class="trip__itinerary-picture">
-
-					<source srcset="https://via.placeholder.com/580x680" media="(min-width: 1000px)">
-
-					<source srcset="https://via.placeholder.com/580x680" media="(min-width: 700px)">
-
-					<img src="https://via.placeholder.com/580x680" alt="A very nice description." class="trip__itinerary-img">
-
-				</picture>
-
-			</div>
-
-			<div class="trip__itinerary-detail">
-
-				<h2 class="trip__itinerary-header">Welcome to the Ireland Crescendo</h2>
-
-				<h3 class="trip__itinerary-subheader">The Itinerary</h3>
-
-				<div class="trip__itinerary-description">
-
-					<p>Featuring the two most sought after tee times in all of Ireland--Royal Portrush & Royal County Down. Add to that the authentic Ireland experience of traveling to the far north of Ireland-- off the 'beaten path' where they say, "It's different up here!"</p>
-
-					<p>Fly overnight from your US Gateway to Dublin. On arrival you head north to Rosapenna to get into position for the week ahead. Check into the Rosapenna Hotel for two nights. While at Rosapenna, play a true hidden gem--Portsalon and the Sandy Hills Course at Rosapenna.</p>
-
-					<p>Now onto Portrush, where you check in for a two night stay. On the way to Portrush you will tee it up at the Glashedy Links at Ballyliffin. While in Portrush, play Portstewart and Royal Portrush (site of the 2019 Open Championship).</p>
-
-					<p>Finally on to Newcastle, home of the beautiful Slieve Donard Hotel and its famous neighbor, Royal County Down Golf Club. Three nights at the Slieve Donard and two more world class rounds of golf--Ardglass and Royal County Down.</p>
-
-					<p>Depart from Dublin at the conclusion of the trip.</p>
+					<?php echo $trip_overview[ 'trip_overview_content' ]; ?>
 
 				</div>
 
@@ -82,7 +24,79 @@
 
 		</div>
 
-	</div>
+	<?php endif; ?>
+
+	<!-- Travefy -->
+
+	<?php $trip_travefy = get_field( 'trip_travefy' ); ?>
+
+	<?php if ( $trip_travefy ) : ?>
+
+		<div class="trip__travefy">
+
+			<div class="inner-wrap trip__travefy-inner-wrap">
+
+				<!-- Use unique ID for content entry -->
+
+				<!-- <iframe src="https://travefy.com/trip/6yw9rqt2mbmwqz2auy7m5xw4932dpfq" class="trip__travefy-iframe"></iframe> -->
+
+				<!-- Use the following... -->
+
+				<div data-travefy-itinerary-id="<?php echo $trip_travefy[ 'trip_travefy_id' ]; ?>" class="travefy-itinerary travefy__itinerary"></div>
+
+				<!-- Add this script to pages that have the data-travefy-itinerary-id code above -->
+
+				<script data-version="v0" id="travefy-itinerary-embedder" type="text/javascript"> (function () { function l() { var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = '//travefy.com/embed/itinerary.1.0.js'; var e = document.getElementById('travefy-itinerary-embedder'); e.parentNode.insertBefore(s, e); } if (window.attachEvent) { window.attachEvent('onload', l); } else { window.addEventListener('load', l, false); } })(); </script>
+
+			</div>
+
+		</div>
+
+	<?php endif; ?>
+
+	<!-- Itenerary -->
+
+	<?php $trip_itinerary = get_field( 'trip_itinerary' ); ?>
+
+	<?php if ( $trip_itinerary ) : ?>
+
+		<div class="trip__itinerary">
+
+			<div class="inner-wrap trip__itinerary-inner-wrap">
+
+				<div class="trip__itinerary-image">
+
+					<picture class="trip__itinerary-picture">
+
+						<source srcset="<?php echo $trip_itinerary[ 'trip_itinerary_image' ]; ?>" media="(min-width: 1000px)">
+
+						<source srcset="<?php echo $trip_itinerary[ 'trip_itinerary_image' ]; ?>" media="(min-width: 700px)">
+
+						<img src="<?php echo $trip_itinerary[ 'trip_itinerary_image' ]; ?>" alt="A very nice description." class="trip__itinerary-img">
+
+					</picture>
+
+				</div>
+
+				<div class="trip__itinerary-detail">
+
+					<h2 class="trip__itinerary-header"><?php echo $trip_itinerary[ 'trip_itinerary_header' ]; ?></h2>
+
+					<h3 class="trip__itinerary-subheader"><?php echo $trip_itinerary[ 'trip_itinerary_subheader' ]; ?></h3>
+
+					<div class="trip__itinerary-description">
+
+						<?php echo $trip_itinerary[ 'trip_itinerary_content' ]; ?>
+
+					</div>
+
+				</div>
+
+			</div>
+
+		</div>
+
+	<?php endif; ?>
 
 	<!-- Locations -->
 
