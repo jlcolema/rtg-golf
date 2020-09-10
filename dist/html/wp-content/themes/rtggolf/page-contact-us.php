67db20de682e71a297b904bd91ad36d68c1aa9f8
Joshua Coleman <jlcolema@me.com>
2020-09-10 11:29:34 -0500

    Connect contact page to the system.

--- dist/html/wp-content/themes/rtggolf/page-contact-us.php
+++ dist/html/wp-content/themes/rtggolf/page-contact-us.php
@@ -2,18 +2,30 @@
 
 	<?php /* Form */ ?>
 
-	<div class="">
+	<?php $contact_us_overview = get_field( 'contact_us_overview' ); ?>
+
+	<?php if ( $contact_us_overview ) : ?>
 
 		<div class="">
 
-			<h2 class="">Please Get in Touch</h2>
+			<div class="">
 
-			<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci sunt quos aliquid cumque ab voluptatum vel expedita debitis quasi doloremque ut hic eum quis, rerum cum labore animi provident qui.</p>
+				<h2 class=""><?php echo $contact_us_overview[ 'contact_us_overview_header' ]; ?></h2>
 
-			<?php echo do_shortcode( '[contact-form-7 id="27" title="Contact"]' ); ?>
+				<h3 class=""><?php echo $contact_us_overview[ 'contact_us_overview_subheader' ]; ?></h3>
+
+				<div class="">
+
+					<?php echo $contact_us_overview[ 'contact_us_overview_content' ]; ?>
+
+				</div>
+
+				<?php echo do_shortcode( '[contact-form-7 id="27" title="Contact"]' ); ?>
+
+			</div>
 
 		</div>
 
-	</div>
+	<?php endif; ?>
 
 <?php get_footer(); ?>
