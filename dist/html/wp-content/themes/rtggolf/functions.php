313a85737d2c6b82ec32843ed8a4786dd990c25f
Joshua Coleman <jlcolema@me.com>
2020-09-10 14:39:24 -0500

    Add support for SVG uploads.

--- dist/html/wp-content/themes/rtggolf/functions.php
+++ dist/html/wp-content/themes/rtggolf/functions.php
@@ -336,6 +336,24 @@ function deregister_features() {
 add_action( 'init', 'deregister_features' );
 
 /*----------------------------------------*\
+   SVG
+\*----------------------------------------*/
+
+// Notes...
+
+function add_file_types_to_uploads($file_types) {
+
+	$new_filetypes = array();
+	$new_filetypes['svg'] = 'image/svg+xml';
+	$file_types = array_merge($file_types, $new_filetypes);
+
+	return $file_types;
+
+}
+
+add_action('upload_mimes', 'add_file_types_to_uploads');
+
+/*----------------------------------------*\
    Contact Form 7
 \*----------------------------------------*/
 
